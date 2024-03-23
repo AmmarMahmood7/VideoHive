01c28ae3587332a7db3e53da5fb3e64aadfbe07b
Ammarmahmood7 <ammarmahmood97@gmail.com>
2024-03-23 12:05:55 +0530

    add a comments container

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -1,11 +1,117 @@
 import React from "react";
 import SingleComment from "./SingleComment";
+import { FaUserCircle } from "react-icons/fa";
 
+const commentsData = [
+  {
+    name: "John Doe",
+    comment: "This is a sample comment",
+    replies: [
+      {
+        name: "john smith",
+        comment: "this is a reply to the sample comment",
+        replies: [
+          {
+            name: "john smith",
+            comment: "this is a reply to the sample comment",
+            replies: [{}],
+          },
+          {
+            name: "jane doe",
+            comment: "this is  reply2 to the sample comment",
+          },
+        ],
+      },
+      {
+        name: "jane doe",
+        comment: "this is  reply2 to the sample comment",
+      },
+    ],
+  },
+  {
+    name: "John Doe",
+    comment: "This is a sample comment",
+    replies: [
+      {
+        name: "john smith",
+        comment: "this is a reply to the sample comment",
+        replies: [
+          {
+            name: "john smith",
+            comment: "this is a reply to the sample comment",
+            replies: [{}],
+          },
+          {
+            name: "jane doe",
+            comment: "this is  reply2 to the sample comment",
+          },
+        ],
+      },
+      {
+        name: "jane doe",
+        comment: "this is  reply2 to the sample comment",
+      },
+    ],
+  },
+  {
+    name: "John Doe",
+    comment: "This is a sample comment",
+    replies: [
+      {
+        name: "john smith",
+        comment: "this is a reply to the sample comment",
+        replies: [
+          {
+            name: "john smith",
+            comment: "this is a reply to the sample comment",
+            replies: [{}],
+          },
+          {
+            name: "jane doe",
+            comment: "this is  reply2 to the sample comment",
+          },
+        ],
+      },
+      {
+        name: "jane doe",
+        comment: "this is  reply2 to the sample comment",
+      },
+    ],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, comment, replies } = data;
+
+  return (
+    <div className="flex p-2 bg-gray-100 rounded-xl font-mono mb-2">
+      <FaUserCircle className="w-8 h-8" />
+      <div className="px-3">
+        <p className="font-bold tracking-wider">{name}</p>
+        <p>{comment}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentsList = ({ commentsData }) => {
+  return (
+    <div>
+      {commentsData.map((comment, index) => {
+        return (
+          <div>
+            <SingleComment data={comment} key={index} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 const CommentsContainer = () => {
   return (
     <div className="p-3 m-3 ">
       <h2 className="font-bold text-2xl mb-3">Comments :</h2>
-      <SingleComment />
+      <CommentsList commentsData={commentsData} />
     </div>
   );
 };
--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
-const SingleComment = () => {
+const SingleComment = ({ data }) => {
+  const { name, comment, replies } = data;
+
   return (
     <div className="flex p-2 bg-gray-100 rounded-xl font-mono mb-2">
       <FaUserCircle className="w-8 h-8" />
       <div className="px-3">
-        <p className="font-bold tracking-wider">Ammar</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, ut?
-        </p>
+        <p className="font-bold tracking-wider">{name}</p>
+        <p>{comment}</p>
       </div>
     </div>
   );
