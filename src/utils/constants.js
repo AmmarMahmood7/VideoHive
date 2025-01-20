0d9c332151100390f22f8fad736ccbee366d8d68
ammarmahmood7 <ammarmahmood97@gmail.com>
2025-01-21 01:06:26 +0530

    removed api key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-console.log("API Key:", API_KEY);
 export const YOUTUBE_POPULAR_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
 
 export const YOUTUBE_SEARCH_API =
